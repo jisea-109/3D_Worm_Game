9b6384b11ea4d1f368d43149887316219d2da185
jisea-109 <77013539+jisea-109@users.noreply.github.com>
2022-01-31 18:20:48 +0000

    error fixed and graphic modification

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -6,53 +6,30 @@ using UnityEngine;
 using static Raycast;
 public class ThirdPersonMovement : MonoBehaviour
 {
+    bool flag = false;
     public CharacterController controller;
     public Transform cam;
 
     private float speed = 6;
     private int gap = 12;
-    private float jumpSpeed;
-    private float ySpeed;
-    private float originalStepOffset;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
 
     private List<Vector3> PositionHistory = new List<Vector3>();
     private List<GameObject> BodyParts = new List<GameObject>();
-    void Start()
-    {
-        originalStepOffset = controller.stepOffset;
-    }
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        ySpeed += Physics.gravity.y * Time.deltaTime;
-        BodyParts = Raycast.bodyMembers;
-        if (controller.isGrounded)
-        {
-            controller.stepOffset = originalStepOffset;
-            ySpeed = -0.5f;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                ySpeed = jumpSpeed;
-            }
-        }
-        else
-        {
-            controller.stepOffset = 0;
-        }
-        Vector3 velocity = direction * direction.magnitude;
-        velocity.y = ySpeed;
-
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
         if (Input.GetMouseButtonDown(0))
         {
             speed = 12;
-            if (gap > 0)
+            if (gap > 1)
             {
                 gap -= 1;
             }
@@ -68,23 +45,18 @@ public class ThirdPersonMovement : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             transform.Rotate(Vector3.up * horizontal * 180 * Time.deltaTime);
-            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
             controller.Move(transform.forward * speed * Time.deltaTime);
             PositionHistory.Insert(0, transform.position);
+            flag = true;
         }
-        if (BodyParts.Count < 5)
+        if (flag == true)
         {
-            for (int i = 0; i <= 4; i++)
+            BodyParts = Raycast.bodyMembers;
+            for (int i = 0; i < BodyParts.Count; i++)
             {
                 Vector3 point = PositionHistory[Mathf.Min(i * gap, PositionHistory.Count - 1)];
                 BodyParts[i].transform.position = point;
             }
-            return;
-        }
-        for (int i = 0; i < BodyParts.Count; i++)
-        {
-            Vector3 point = PositionHistory[Mathf.Min(i * gap, PositionHistory.Count - 1)];
-            BodyParts[i].transform.position = point;
         }
     }
 }
