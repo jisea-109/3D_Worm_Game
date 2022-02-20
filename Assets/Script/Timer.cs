efde6815d3aab1dac3b82466731d1f8d8168d4e2
jisea-109 <77013539+jisea-109@users.noreply.github.com>
2022-02-20 22:09:27 +0000

    edited timer

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -7,11 +7,13 @@ public class Timer : MonoBehaviour
     public Text score;
     public float time = 0f;
     public int minutes;
+    public int seconds;
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        minutes = Mathf.RoundToInt(time / 60);
-        score.text = "Time: " + minutes.ToString("00") + ": " + time.ToString("00");
+        minutes = Mathf.FloorToInt(time / 60);
+        seconds = Mathf.FloorToInt(time % 60);
+        score.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
